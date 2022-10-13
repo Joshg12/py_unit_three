c36ad7a5b1c78d4f2206dbd097044fd4d60f7cbd
Joshg12 <112645058+Joshg12@users.noreply.github.com>
2022-10-13 11:54:00 -0400

    3.7(New)

---
+++ assignment_three.py
@@ -0,0 +1,18 @@
+#Joshua A Goldstein
+
+#10/13/22
+
+#Area of a rectangular prism calculator
+
+
+def areacalc(legnth, width):
+    area = legnth * width
+    return area
+
+
+
+legnth = 4
+width = 3
+area = areacalc(legnth, width)
+
+print(area)
