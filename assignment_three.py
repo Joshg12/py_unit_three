485cc61804af360f05c01f06d5a5db10ba8b9bd9
Joshg12 <112645058+Joshg12@users.noreply.github.com>
2022-10-14 14:05:21 -0400

    3.7(New)

--- assignment_three.py
+++ assignment_three.py
@@ -9,13 +9,38 @@ def areacalc(legnth, width):
     return legnth * width
 
 
-
+'''
 legnth = 4
 width = 3
+height = 9
+
+
+'''
+
+def userwidth():
+
+def userheight():
+def userlegnth():
+
+
 area = areacalc(legnth, width)
 
-print(area)
+print("The area is: ",area)
 
 
-def totalsurfacearea(legnth, widht, height):
-    return legnth * width * height
+def rectangle_area(side1, side2):
+    return side1 * side2
+def surfacearea(height, length, width):
+    top = rectangle_area(length, width)
+    bottom = rectangle_area(length, width)
+    front = rectangle_area(height, length)
+    back = rectangle_area(height, length)
+    left = rectangle_area(height, width)
+    right = rectangle_area(height, width)
+    total = (top + bottom + front + back + left + right)
+    return total
+
+
+
+totalArea = surfacearea(height, legnth, width)
+print (totalArea)
